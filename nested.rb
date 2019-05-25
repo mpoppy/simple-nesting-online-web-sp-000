c3120de477f84fbc379e293d3bd19b2a174dba7a
Marc Poppy <poppy.marc@gmail.com>
2019-05-25 20:10:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 language = programmer_hash[:dennis_ritchie][:languages]
-		 
+		 programmer_hash.to_s[:dennis_ritchie][:languages]
+
 end
 
 def adding_matz
